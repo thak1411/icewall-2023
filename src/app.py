885dd50d350a3cafd821e93a3d273522f62457c9
thak1411 <thak1411@gmail.com>
2023-09-26 19:27:11 +0900

    Add: assignment 3

--- src/app.py
+++ src/app.py
@@ -1,11 +1,19 @@
-import os
-from flask import Flask, request, render_template, redirect 
+from flask import Flask, request, render_template, redirect, session
+from dotenv import dotenv_values
 from models import db, User
+from pathlib import Path
+
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Icewall!' # Hello, Icewall! 
+    if 'username' not in session:
+        return redirect('/login/')
+    else:
+        username = session['username']
+        return 'Hello, ' + username
 
 @app.route('/signup/', methods= ['GET', 'POST'])
 def signup():
@@ -14,7 +22,7 @@ def signup():
         password = request.form.get('password')
 
         if not (username):
-            return "사용자 이름이 입력되지 않았습니다"
+            return '사용자 이름이 입력되지 않았습니다'
         else:
             usertable=User()
             usertable.username = username
@@ -22,15 +30,37 @@ def signup():
             
             db.session.add(usertable)
             db.session.commit()
-            return "회원가입 성공"
+            return '회원가입 성공'
     
     else:
-        return render_template("signup.html")
+        return render_template('signup.html')
 
-if __name__ == '__main__':
+@app.route('/login/', methods= ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if not (username and password):
+            return '아이디 혹은 패스워드를 입력해 주세요.'
+        else:
+            user = User.query.filter_by(username=username).first()
+            if user is not None:
+                session['username'] = username
+                return redirect('/')
+            else:
+                return '아이디 혹은 패스워드가 올바르지 않습니다.'
+    else:
+        return render_template('login.html')
+
+def main():
+    dotenv_path = Path(__file__).parent.parent / '.env'
+    config = dotenv_values(dotenv_path)
+
     with app.app_context():
         basedir = os.path.abspath(os.path.dirname(__file__))
         dbfile = os.path.join(basedir, 'db.sqlite')
+        app.config['SECRET_KEY'] = config.get('SECRET_KEY', 'ICEWALL')
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
         app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -40,3 +70,6 @@ if __name__ == '__main__':
         db.create_all()
 
         app.run(host='127.0.0.1', port=5000, debug=True)
+
+if __name__ == '__main__':
+    main()
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,5 +7,13 @@ class User(db.Model):
     _tablename = 'user' #테이블 이름
 
     id = db.Column(db.Integer, primary_key=True) # 자동으로 증가하는 User 모델의 기본 키 
-    username = db.Column (db.String (100), unique=True, nullable=False) # 같은 값 저장 X, 빈 값 X
-    password = db.Column(db.String (20), nullable=False) # 빈값 X
+    username = db.Column(db.String(100), unique=True, nullable=False) # 같은 값 저장 X, 빈 값 X
+    password = db.Column(db.String(20), nullable=False) # 빈값 X
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    content = db.Column(db.String(1000))
+    created_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
